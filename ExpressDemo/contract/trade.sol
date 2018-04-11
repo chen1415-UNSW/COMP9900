a6cb9a5c01e9f9c357706288b456e714525cca30
Yikhan <yhxiaoch@gmail.com>
2018-04-11 10:46:53 +1000

    executable contract with basic functions

--- ExpressDemo/contract/trade.sol
+++ ExpressDemo/contract/trade.sol
@@ -8,28 +8,25 @@ contract Trade {
    string item;
    uint price;
    
-   string seller_hash;
-   string buyer_hash;
-   string item_hash;
-   
+   event showTrade(
+       string seller,
+       string buyer,
+       string item,
+       uint price
+       
+   );
+
    function setTrade(string _seller, string _buyer, string _item, uint _price) public {
        seller = _seller;
        buyer = _buyer;
        item = _item;
        price = _price;
+       showTrade(_seller, _buyer, _item, _price);
    }
    
    function getTrade() public constant returns (string, string, string, uint) {
        return (seller, buyer, item, price);
    }
     
-   function setHash(string _seller_hash, string _buyer_hash, string _item_hash ) public {
-       seller_hash = _seller_hash;
-       buyer_hash = _buyer_hash;
-       item_hash = _item_hash;
-   }
-    
-   function getHash() public constant returns (string, string, string) {
-       return (seller_hash, buyer_hash, item_hash);
-   }    
+  
 }

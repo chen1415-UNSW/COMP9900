c84d752970b8dccb5f1b43988fd0dd774d9d6582
YIKHAN-NOTEBOOK\Yifelse <yhxiaoch@gmail.com>
2018-04-18 17:06:29 +1000

    fix bugs that index router fail to work when enter the website

--- NewBlock/routes/index.js
+++ NewBlock/routes/index.js
@@ -1,14 +1,11 @@
-// module.exports = function (request, response, next) {
-//     response.render('index');
-//
-// };
-
-
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+
 router.get('/index', function(req, res, next) {
     res.render('index');
 });

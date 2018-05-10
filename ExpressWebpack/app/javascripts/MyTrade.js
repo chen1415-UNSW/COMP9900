81e334f188593e9c930365ae51e134d2500e36ee
Yikhan <yhxiaoch@gmail.com>
2018-05-11 00:18:14 +1000

    smart contract v2.0

--- ExpressWebpack/app/javascripts/MyTrade.js
+++ ExpressWebpack/app/javascripts/MyTrade.js
@@ -13,12 +13,19 @@ var default_buyer
 var default_seller
 var buyer_balance
 var seller_balance
+// 合约实例化
+var instance
+// 合约默认部署地址
+var contractAddress
+// 合约余额
+var contractBalance
 
 window.App = {
 
-    start: function() {
+    start: async function() {
         var self = this
         MyTrade.setProvider(web3.currentProvider)
+        instance = await MyTrade.deployed()
 
         web3.eth.getAccounts(function(err, accs) {
             if (err != null) {
@@ -32,7 +39,7 @@ window.App = {
             }
             // 以后需要替换为用户的实际账户
             account_list = accs
-            default_buyer = account_list[0]
+            default_buyer = account_list[2]
             default_seller = account_list[1]
            
             console.log("Check account")
@@ -42,10 +49,14 @@ window.App = {
             web3.eth.defaultAccount = default_buyer
 
             self.refreshBalance(default_buyer)
-
-           
+  
           })
-    
+        // 得到Trade合约地址
+        self.getContractAddress()
+       
+        // 注册事件
+        self.tradeEvent()
+
     },
 
     // 返回新的地址给注册的新用户
@@ -73,6 +84,18 @@ window.App = {
         
     },
 
+    getCurrentIndexFromBlock: async function() {
+        var value = await instance.getCurrentTradeIndex()
+        console.log("Current Trade Index On Block: " + value)
+        return parseInt(value)
+    },
+
+    getContractAddress: async function() {
+        contractAddress = await instance.getContractAddress()   
+        console.log("Show contract address")
+        console.log(contractAddress)
+    },
+
     checkBalanceEnough: function(account, total) {
         var self = this
         if (total < self.refreshBalance(account)) {
@@ -83,15 +106,18 @@ window.App = {
         }
     },
 
-    makeTrade: function(cart_list, callback) {
+    makeTrade: async function(cart_list, callback) {
 
         var self = this
         var txHashList = []
-        var transaction_num = cart_list.length
         var waiting_for = 0 //异步等待标志
         var flag = true
+
+        var cartSize = cart_list.length
+        var lastMaxIndex = await self.getCurrentIndexFromBlock()
+
         function waitingEnd(flag) {
-            if (flag === transaction_num) {
+            if (flag === cartSize) {
                 callback(flag, cart_list)
             }
         }
@@ -100,79 +126,95 @@ window.App = {
         for (var i in cart_list){
             total += cart_list[i].number * cart_list[i].price
         }
-        // 尚未添加价格确认
+        //尚未添加价格确认
 
+        contractBalance = web3.eth.getBalance(contractAddress).toNumber()
+        console.log("Show contract balance before trade")
+        console.log(contractBalance)
+
+        try{
         for (var i in cart_list) {
-            MyTrade.deployed().then(function(instance) {
+            ( async function(i){ 
                 // 如果买卖双方地址没有被赋值，使用默认地址交易
-                var tradeDetail = cart_list[i]
+                let tradeDetail = cart_list[i]
                 if (tradeDetail.buyerAddress === undefined) {
                     tradeDetail.buyerAddress = default_buyer
                 }
                 if (tradeDetail.sellerAddress === undefined) {
                     tradeDetail.sellerAddress = default_seller
                 }
+                
                 console.log("Show trade addresses:")
                 console.log(tradeDetail.buyerAddress)
                 console.log(tradeDetail.sellerAddress)
-                //
-                var contractAddress = instance.getContractAddress().value
-                var contractBalance = instance.getContractBalance()
-                console.log("Show contract info")
-                console.log(contractAddress)
+            
+                // 该商品的总价
+                let total_ThisItem = tradeDetail.productPrice * tradeDetail.number
+                // 向区块发送
+                let value = await instance.setTrade(
+                    tradeDetail.selleruid, 
+                    tradeDetail.uid, 
+                    tradeDetail.pid,
+                    tradeDetail.number, 
+                    web3.toWei(tradeDetail.productPrice, 'ether'),
+                    web3.toWei(total_ThisItem, 'ether'),
+                    tradeDetail.sellerAddress, 
+                    {from:tradeDetail.buyerAddress, value:web3.toWei(1, 'ether'), gas:3000000})
+
+                console.log("Waiting for transaction:" + i)
+                console.log(value.tx)
+                let currentIndex = lastMaxIndex + parseInt(i)
+
+                console.log("Trade Index here: " + currentIndex)
+                //在输入的购物车里面添加交易成功的Hash
+                cart_list[i].hash = value.tx
+
+                contractBalance = web3.eth.getBalance(contractAddress).toNumber()
+                console.log("Show contract balance after trade")
                 console.log(contractBalance)
 
-                var txHash = instance.setTrade.sendTransaction(tradeDetail.selleruid, tradeDetail.uid, tradeDetail.pid,
-                    tradeDetail.number, web3.toWei(tradeDetail.productPrice, 'ether')
-                    , tradeDetail.sellerAddress, 
-                    {from:tradeDetail.buyerAddress, value:web3.toWei(total, 'ether'), gas:3000000})
+                //再把钱付给卖家
+                //var payHash = await instance.confirmTrade(currentIndex)
+                var payHash = await instance.confirmTradeByAddress(
+                "0x60EC8abbb9d6C807B0F2cd3D1E39c7D103EaF2f1", 
+                {from:tradeDetail.buyerAddress})
 
-                txHash.then(function(value) {
-                    console.log("Waiting for transaction:")
-                    console.log(value.tx)
-                    //在输入的购物车里面添加交易成功的Hash
-                   
-                    var contractBalance = instance.getContractBalance()
-                    console.log("Show contract balance")
-                    
-                    console.log(contractBalance)
-
-                    tradeDetail.hash = value.tx
-                    waiting_for += 1
-                    waitingEnd(waiting_for)
-                })
-            }).then(function() {
+                waiting_for += 1
+                waitingEnd(waiting_for)
                 console.log("Trade Complete!")
-                
-            }).catch(function(e) {
-                console.log(e)
-                console.log("Error making trade, check log")
-            })
-        }
-       
+            })(i)
+        } 
+        } catch(e) {
+            console.log(e)
+            console.log("Error making trade, check log")
+        }   
     },
 
-    testTrade: function() {
-        var tempAddr = "0x2b98f779b6eb273caa9cf73607be899625c3ac6d"
-        MyTrade.deployed().then(function(instance) {
-            var contractAddress = instance.getContractAddress()
-            contractAddress.then(function(value) {
-                var testAddr = value.toString()
-                console.log("Contract Address")
-                console.log(value)
-                console.log(typeof(value))
+    testTrade: async function() {
+        var tempAddr = "0x60EC8abbb9d6C807B0F2cd3D1E39c7D103EaF2f1"
+        var contractAddress = await instance.getContractAddress()
+        contractAddress.then(function(value) {
+            var testAddr = value.toString()
+            console.log("Contract Address")
+            console.log(value)
+            console.log(typeof(value))
+            var contractBalance = web3.eth.getBalance(testAddr).toNumber()
+            console.log("Show contract info before trade")
+            console.log(contractBalance)
+        
+            async function makeBuy(){
+                var txHash = await instance.setBuy(
+                    {from:tempAddr, value:web3.toWei(2, 'ether')}
+                    )
+                console.log(txHash)
                 var contractBalance = web3.eth.getBalance(testAddr).toNumber()
                 console.log("Show contract info before trade")
                 console.log(contractBalance)
-                var txHash = web3.eth.sendTransaction(
-                    {from:default_buyer, to:testAddr, value:web3.toWei(5, 'ether')}, function(){
-                        contractBalance = web3.eth.getBalance(testAddr).toNumber()
-                        console.log("Show contract info after trade")
-                        console.log(contractBalance)
-                    })
-            })
-           
-        })
+
+            }
+            makeBuy()
+            
+        })    
     },
 
     readTransaction: function(address) {
@@ -180,13 +222,16 @@ window.App = {
     },
 
     tradeEvent: function() {
-        MyTrade.deployed().then(function(instance) {
-            instance.showTrade().watch(function(error, result) {
-                console.log("Seller:" + result.args.seller + " Buyer:" + result.args.buyer + 
-                " Product:" + result.args.item)
-            })
+        instance.showTrade().watch(function(error, result) {
+            console.log("Seller:" + result.args.seller + " Buyer:" + result.args.buyer + 
+            " Product:" + result.args.item)
         })
-        
+        instance.showTradeIndex().watch( async function(error, result){
+            let index = result.args.single_index.toNumber()
+            console.log("Record trade index: " + index)
+            let value = await instance.getSellerAddress(index)
+            console.log(value)
+        })   
     }
 
 }
--- ExpressWebpack/contracts/Trade.sol
+++ ExpressWebpack/contracts/Trade.sol
@@ -4,13 +4,19 @@ import "./SafeMath.sol";
 
 contract Trade is SafeMath {
     
-    string seller;
-    string buyer;
-    string item;
-    uint number;
-    uint price;
-    uint total;
-    address receiver;
+    
+    struct SingleTrade {
+        string seller;
+        string buyer;
+        string item;
+        uint number;
+        uint price;
+        uint total;
+        address seller_addr;
+    }
+
+    uint totalTrades;
+    mapping(uint => SingleTrade) TradeList;
 
     event showTrade(
         string seller,
@@ -18,37 +24,80 @@ contract Trade is SafeMath {
         string item,
         uint number,
         uint price,
-        uint total
-       
+        uint total,
+        address seller_addr
+    );
+
+    event showTradeIndex(
+        // 交易在合约记录中的编号，返回后方便用户确认付款
+        uint single_index
+    );
+
+    event showBuy(
+        address sender,
+        bytes data
     );
 
     function () payable public{
         
-    }  
+    } 
+
+    function setBuy() payable public returns (bool) {
+        emit showBuy(msg.sender, msg.data);
+        return(true);
+    }
     
     function getContractAddress() public view returns (address) {
         return(address(this));
     }
+
+    function confirmTrade(uint tradeIndex) public returns (bool) {
+        address(TradeList[tradeIndex].seller_addr).transfer(1 ether);
+        return(true);
+    }
+
+    function confirmTradeByAddress(address seller_addr) public returns (bool) {
+        address(seller_addr).transfer(1 ether);
+        return(true);
+    }
     
-    function setTrade(string _seller, string _buyer, string _item, uint _number, uint _price, address _receiver) public {
-        seller = _seller;
-        buyer = _buyer;
-        item = _item;
-        number = _number;
-        price = _price;
-        total = safeMul(number, price);
+    function setTrade(
+        string _seller, string _buyer, string _item, uint _number, uint _price, uint _total, address _receiver)
+        payable public returns (bool){
+        
+        // seller = _seller;
+        // buyer = _buyer;
+        // item = _item;
+        // number = _number;
+        // price = _price;
+        // total = safeMul(number, price);
+        // seller_addr = _receiver;
         // if (address(msg.sender).balance < total) {
         //     return;
         // } else {
-        //     address(_receiver).transfer(total);
-       
-            
+        //   address(_receiver).transfer(total);   
         // }
-        emit showTrade(seller, buyer, item, number, price, total);
+        // emit showTrade(_seller, _buyer, _item, _number, _price, _total);
+        TradeList[totalTrades] = SingleTrade(_seller, _buyer, _item,  _number, _price, _total, _receiver);
+        emit showTradeIndex(totalTrades);
+        
+        totalTrades++;
+
+        return(true);
     }
 
-    function getTrade() public view returns (string, string, string, uint, uint, uint) {
-        return (seller, buyer, item, number, price, total);
+    function getTrade(uint tradeIndex) public view returns (string, string, string, uint, uint, uint) {
+        SingleTrade memory s = TradeList[tradeIndex];
+        return(s.seller, s.buyer, s.item, s.number, s.price, s.total);
+    }
+
+    function getSellerAddress(uint tradeIndex) public view returns (address) {
+        SingleTrade memory s = TradeList[tradeIndex];
+        return(s.seller_addr);
+    }
+
+    function getCurrentTradeIndex() public view returns (uint) {
+        return(totalTrades);
     }
 
 

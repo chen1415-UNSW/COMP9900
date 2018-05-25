2cadc28b00ab3d18d09084c178356b97b8be3f30
Yikhan <yhxiaoch@gmail.com>
2018-05-25 10:58:18 +1000

    Rename directory

--- BlockBusiness/routes/ViewOrderSelling.js
+++ BlockBusiness/routes/ViewOrderSelling.js
@@ -14,14 +14,14 @@ module.exports = function (request, response, next) {
     }
     else
     {
-        console.log("可以进入orderhistorySale页面");
+        console.log("Enter orderhistorySale");
         console.log("Profile Control:  UID   ", request.session.userid.uid);
 
         Blocks.find({'selleruid':request.session.userid.uid}, function(err, res){
 
             ViewOrderList = res;
-            console.log("============block resultList===111=========");
-            console.log(ViewOrderList);
+            console.log("============block resultList============");
+            //console.log(ViewOrderList);
 
             response.render('orderHistorySale', {title:'View History Page', u_name:request.session.user.username,
                 uid:request.session.userid.uid, result:ViewOrderList});
